332ff81076aab93b55b92de5266307ad6b380c64
Eden Park <edengoforit@gmail.com>
2025-03-14 17:51:33 +1300

    feat: identity package added

--- Momentum/Momentum.Application/Dtos/Users/UserDto.cs
+++ Momentum/Momentum.Application/Dtos/Users/UserDto.cs
@@ -3,5 +3,10 @@ namespace Momentum.Application.Dtos.Users;
 public class UserDto
 {
     public required string UserName { get; set; } = string.Empty;
-    public required string PasswordHash { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public required string Password { get; set; } = string.Empty;
+
+    public UserDto()
+    {
+    }
 }
--- Momentum/Momentum.Application/Users/Commands/CreateUser/CreateUserCommand.cs
+++ Momentum/Momentum.Application/Users/Commands/CreateUser/CreateUserCommand.cs
@@ -1,4 +1,5 @@
 using MediatR;
+using Microsoft.AspNetCore.Identity;
 using Momentum.Application.Abstractions;
 using Momentum.Application.Dtos.Users;
 using Momentum.Domain.Errors;
@@ -10,11 +11,19 @@ namespace Momentum.Application.Users.Commands.CreateUser;
 public record CreateUserCommand : ICommand<long>
 {
     public string UserName { get; set; } = string.Empty;
-    public string PasswordHash { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
 }
 
 public class CreateUserCommandHandler : ICommandHandler<CreateUserCommand, long>
 {
-    public Task<Result<long, IDomainError>> Handle(CreateUserCommand request, CancellationToken cancellationToken) =>
-        Task.FromResult(Result.Success<long, IDomainError>(1));
+    public Task<Result<long, IDomainError>> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        var user = new UserDto();
+
+        string hashedPassword = new PasswordHasher<UserDto>().HashPassword(user, request.Password);
+        user.UserName = request.UserName;
+        user.PasswordHash = hashedPassword;
+
+        return Task.FromResult(Result.Success<long, IDomainError>(1));
+    }
 }
