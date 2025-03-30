70650a5584bc70888671841f28b9b6ace7dca886
Eden Park <edengoforit@gmail.com>
2025-03-30 20:51:29 +1300

    feat: signin image and some images added

--- Momentum/Momentum.Presentation/Momentum.Mobile/app/(root)/(tabs)/index.tsx
+++ Momentum/Momentum.Presentation/Momentum.Mobile/app/(root)/(tabs)/index.tsx
@@ -1,17 +1,12 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Redirect } from "expo-router";
+import React from "react";
 
-export default function Index() {
+interface IHomeProps {}
+
+export default function Home() {
   return (
-    <View>
-      <Link href="/sign-in">SignIn</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href="/habit/1">Habit</Link>
-      <Link href="/sign-in">SignIn</Link>
-      <Text className="font-bold text-lg my-3 bg-red-500  border-white py-2">
-        Edit app/index.tsx to edit this screen.
-      </Text>
-    </View>
+    <>
+      <Redirect href="/sign-in" />
+    </>
   );
 }
--- Momentum/Momentum.Presentation/Momentum.Mobile/app/sign-in.tsx
+++ Momentum/Momentum.Presentation/Momentum.Mobile/app/sign-in.tsx
@@ -1,10 +1,18 @@
+import images from "@/constants/images";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function signIn() {
   return (
-    <View>
-      <Text>sign-in</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.signIn}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
