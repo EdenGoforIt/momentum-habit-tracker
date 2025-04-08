2d59ba5bb21ecf30765096d053539291c9da4326
Eden Park <edengoforit@gmail.com>
2025-04-08 19:44:56 +1200

    feat: add auth as group to the identity map

--- Momentum/Momentum.Api/Program.cs
+++ Momentum/Momentum.Api/Program.cs
@@ -25,7 +25,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
-app.MapIdentityApi<IdentityUser>();
+app.MapGroup("/auth").MapIdentityApi<IdentityUser>();
 
 app.MapGet("/endpoints", (IEnumerable<EndpointDataSource> endpointSources) =>
 {
