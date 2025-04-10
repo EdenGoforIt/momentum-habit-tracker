7dc9d436adcae89ee09372dd0e5fe97f93a7f5a8
Eden Park <edengoforit@gmail.com>
2025-04-10 17:32:03 +1200

    feat: add add habit

---
+++ Momentum/Momentum.Presentation/Momentum.Mobile/app/(protected)/habit/add.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function addHabit() {
+  return (
+    <View>
+      <Text>Add Habit</Text>
+    </View>
+  );
+}
