54d8bd62a791b44c6dce5c1e3e6da4baaf65f6d0
Eden Park <edengoforit@gmail.com>
2025-06-10 21:02:18 +1200

    feat: change habits

--- Momentum/Momentum.Application/Habits/GetAll/GetHabitsQuery.cs
+++ Momentum/Momentum.Application/Habits/GetAll/GetHabitsQuery.cs
@@ -1,9 +1,23 @@
 using Momentum.Application.Abstractions;
 using Momentum.Application.Dtos.Habit;
+using Momentum.Domain.Errors;
+using Momentum.Infrastructure.Data;
 
 namespace Momentum.Application.Habits.GetAll;
 
-public abstract class GetHabitsQuery : IQuery<string>
+public abstract class GetHabitsQuery : IQuery<IEnumerable<HabitDto>>
 {
     public required string Id { get; set; }
+}
+
+// ReSharper disable once HollowTypeName
+public class GetHabitsQueryHandler(DataContext context, IMapper mapper)
+    : IQueryHandler<GetHabitsQuery, IEnumerable<HabitDto>>
+{
+    private readonly IMapper _mapper = Guard.Against.Null(mapper, nameof(IMapper));
+    private readonly DataContext _context = Guard.Against.Null(context, nameof(AppContext));
+    public Task<Result<IEnumerable<HabitDto>, IDomainError>> Handle(GetHabitsQuery request,
+        CancellationToken cancellationToken)
+    {
+    }
 }
