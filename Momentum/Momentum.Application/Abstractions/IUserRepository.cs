f5862c2929e986064919ea67f58021f1ec2d4a3f
Eden Park <edengoforit@gmail.com>
2025-06-16 18:26:50 +1200

    feat: IUserRepository.cs

--- Momentum/Momentum.Application/Abstractions/IUserRepository.cs
+++ Momentum/Momentum.Application/Abstractions/IUserRepository.cs
@@ -1,6 +1,8 @@
+using Momentum.Domain.Entities.Auth;
+
 namespace Momentum.Application.Abstractions;
 
-public interface IUserRepository
+public interface IUserRepository : IRepository<User>
 {
     Task<bool> UserExistsAsync(string userId, CancellationToken cancellationToken);
 }
