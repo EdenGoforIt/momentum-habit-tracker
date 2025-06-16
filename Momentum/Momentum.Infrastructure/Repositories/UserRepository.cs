7692a00a2e9dbeed238f4bacdf9618389a37b7a8
Eden Park <edengoforit@gmail.com>
2025-06-16 18:27:03 +1200

    feat: user repository

--- Momentum/Momentum.Infrastructure/Repositories/UserRepository.cs
+++ Momentum/Momentum.Infrastructure/Repositories/UserRepository.cs
@@ -1,6 +1,31 @@
+using Momentum.Application.Abstractions;
+using Momentum.Domain.Entities.Auth;
+
 namespace Momentum.Infrastructure.Repositories;
 
-public class UserRepository:IUserRepository
+public class UserRepository : IUserRepository
 {
-    
+    public Task<IEnumerable<User>> GetAllAsync(CancellationToken cancellationToken = default) =>
+        throw new NotImplementedException();
+
+    public Task<User?> GetByIdAsync(object id, CancellationToken cancellationToken = default) =>
+        throw new NotImplementedException();
+
+    public void Add(User entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(User entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(User entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UserExistsAsync(string userId, CancellationToken cancellationToken) =>
+        throw new NotImplementedException();
 }
