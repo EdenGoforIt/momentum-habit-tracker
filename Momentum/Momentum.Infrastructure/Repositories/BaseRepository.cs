78678cc920be46505644f27be6f834fe9966a7c4
Eden Park <edengoforit@gmail.com>
2025-06-18 19:56:53 +1200

    feat: base repo added

--- Momentum/Momentum.Infrastructure/Repositories/BaseRepository.cs
+++ Momentum/Momentum.Infrastructure/Repositories/BaseRepository.cs
@@ -1,6 +1,28 @@
+using Momentum.Infrastructure.Data;
+
 namespace Momentum.Infrastructure.Repositories;
 
-public class BaseRepository
+public class BaseRepository<T> where T : class
 {
-    
+    private readonly DataContext _context;
+
+    public BaseRepository(DataContext context)
+    {
+        _context = context;
+    }
+
+    public virtual void Add(T entity)
+    {
+        _context.Set<T>().Add(entity);
+    }
+
+    public virtual void Update(T entity)
+    {
+        _context.Set<T>().Update(entity);
+    }
+
+    public virtual void Delete(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+    }
 }
