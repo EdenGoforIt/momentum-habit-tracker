cac8133ba4482765a63493ea587abe1727350ccd
Eden Park <edengoforit@gmail.com>
2025-06-28 18:06:53 +1200

    feat: move the validator

--- Momentum/Momentum.Application/Dtos/Habit/HabitDto.cs
+++ Momentum/Momentum.Application/Dtos/Habit/HabitDto.cs
@@ -26,23 +26,3 @@ public class HabitDto
 
     public ICollection<HabitEntryDto> HabitEntries { get; } = [];
 }
-
-public class HabitDtoValidator : AbstractValidator<HabitDto>
-{
-    public HabitDtoValidator()
-    {
-        RuleFor(x => x.Name)
-            .NotEmpty().WithMessage("Habit name is required.")
-            .MaximumLength(100).WithMessage("Habit name must not exceed 100 characters.");
-        RuleFor(x => x.Description)
-            .MaximumLength(500).WithMessage("Description must not exceed 500 characters.");
-        RuleFor(x => x.Frequency).IsInEnum()
-            .WithMessage("Frequency must be a valid HabitFrequency enum value.");
-        RuleFor(x => x.UserId)
-            .NotEmpty().WithMessage("User ID is required.")
-            .MaximumLength(50).WithMessage("User ID must not exceed 50 characters.");
-        RuleFor(x => x.CategoryId)
-            .GreaterThan(0).When(x => x.CategoryId.HasValue)
-            .WithMessage("Category ID must be greater than 0 if provided.");
-    }
-}
--- Momentum/Momentum.Application/Validators/Habit/HabitDtoValidator.cs
+++ Momentum/Momentum.Application/Validators/Habit/HabitDtoValidator.cs
@@ -1,6 +1,25 @@
+using FluentValidation;
+using Momentum.Application.Dtos.Habit;
+
 namespace Momentum.Application.Validators.Habit;
 
-public class HabitDtoValidator
+
+public class HabitDtoValidator : AbstractValidator<HabitDto>
 {
-    
+    public HabitDtoValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Habit name is required.")
+            .MaximumLength(100).WithMessage("Habit name must not exceed 100 characters.");
+        RuleFor(x => x.Description)
+            .MaximumLength(500).WithMessage("Description must not exceed 500 characters.");
+        RuleFor(x => x.Frequency).IsInEnum()
+            .WithMessage("Frequency must be a valid HabitFrequency enum value.");
+        RuleFor(x => x.UserId)
+            .NotEmpty().WithMessage("User ID is required.")
+            .MaximumLength(50).WithMessage("User ID must not exceed 50 characters.");
+        RuleFor(x => x.CategoryId)
+            .GreaterThan(0).When(x => x.CategoryId.HasValue)
+            .WithMessage("Category ID must be greater than 0 if provided.");
+    }
 }
