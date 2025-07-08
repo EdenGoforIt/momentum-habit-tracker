f3b2235d4ea2aaa646254dbeeaa8d774ce5e4943
Eden Park <edengoforit@gmail.com>
2025-07-08 20:12:54 +1200

    feat: add entities

--- Momentum/Momentum.Infrastructure/Data/DataContext.cs
+++ Momentum/Momentum.Infrastructure/Data/DataContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Momentum.Domain.Entities.Achievements;
 using Momentum.Domain.Entities.Auth;
 using Momentum.Domain.Entities.Habits;
 
@@ -11,7 +12,15 @@ public class DataContext(DbContextOptions<DataContext> options) : IdentityDbCont
     public DbSet<Reminder> Reminders { get; set; }
     public DbSet<Category> Categories { get; set; }
     public DbSet<HabitEntry> HabitEntries { get; set; }
+    public DbSet<Achievement> Achievements { get; set; }
+    public DbSet<UserAchievement> UserAchievements { get; set; } 
+    public DbSet<HabitGoal> HabitGoals { get; set; } 
+    public DbSet<HabitSchedule> HabitSchedules { get; set; } 
 
+    /// <summary>
+    /// This method is called when the model for a derived context is being created.
+    /// </summary>
+    /// <param name="builder">The builder used to construct the model for this context.</param>
     protected override void OnModelCreating(ModelBuilder builder)
     {
         ArgumentNullException.ThrowIfNull(builder);
