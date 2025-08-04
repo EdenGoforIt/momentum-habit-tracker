3bd88a31e841de9c482c4424381abf349bd95880
Eden Park <edengoforit@gmail.com>
2025-08-04 19:40:00 +1200

    feat: remove unused routes

--- Momentum/Momentum.Presentation/Momentum.Mobile/app/(protected)/profile/_layout.tsx
+++ Momentum/Momentum.Presentation/Momentum.Mobile/app/(protected)/profile/_layout.tsx
@@ -15,21 +15,7 @@ export default function ProfileLayout() {
           header: () => <Header title="Profile" showClose={true} />,
         }}
       />
-      <Stack.Screen
-        name="change-password" // This implies app/(profile)/change-password.tsx
-        options={{
-          header: () => (
-            <Header title="Change Password" showBack={true} showClose={true} />
-          ),
-        }}
-      />
-      {/* ... and so on for your other Stack.Screen components */}
-      <Stack.Screen
-        name="edit" // This implies app/(profile)/edit.tsx
-        options={{
-          header: () => <Header title="Edit Profile" showClose={true} />,
-        }}
-      />
+
       <Stack.Screen
         name="support" // This implies app/(profile)/support.tsx
         options={{
@@ -40,18 +26,6 @@ export default function ProfileLayout() {
         name="about" // This implies app/(profile)/about.tsx
         options={{
           header: () => <Header title="About Momentum" showClose={true} />,
-        }}
-      />
-      <Stack.Screen
-        name="privacy-policy" // This implies app/(profile)/privacy-policy.tsx
-        options={{
-          header: () => <Header title="Privacy Policy" showClose={true} />,
-        }}
-      />
-      <Stack.Screen
-        name="update-email" // This implies app/(profile)/update-email.tsx
-        options={{
-          header: () => <Header title="Update Email" showClose={true} />,
         }}
       />
     </Stack>
